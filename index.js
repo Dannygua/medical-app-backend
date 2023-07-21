377f57ceaa00e88b54b729d327603d95ab75faa2
EzqTrujillo55 <pablo.trujillo@epn.edu.ec>
2023-07-20 21:16:26 -0500

    Agora genrate tokens

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/usersRoutes.js";
 import recordRoutes from "./routes/recordsRoutes.js";
 import datesRoutes from "./routes/datesRoutes.js";
 import frequentQuestionsRoutes from "./routes/frequentQuestionsRoutes.js";
+import agoraRoutes from "./routes/agoraRoutes.js";
 import cors from "cors";
 import bodyParser from "body-parser";
 import multer from "multer";
@@ -21,11 +22,8 @@ const whitelist = [process.env.FRONTEND_URL, process.env.FRONTEND_URL_LOCAL];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Error de Cors"));
-    }
+    callback(null, true);
+
   },
 };
 
@@ -35,6 +33,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/records", recordRoutes);
 app.use("/api/dates", datesRoutes);
 app.use("/api/frequentQuestions", frequentQuestionsRoutes);
+app.use("/api/agora", agoraRoutes);
 
 const PORT = process.env.PORT || 4000;
 
