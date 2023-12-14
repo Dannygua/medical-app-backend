06bff60edb4345ca2ef08be9af5c3154aa9ebb6d
EzqTrujillo55 <pablo.trujillo@epn.edu.ec>
2023-12-14 13:06:26 -0500

    New env

--- controllers/envsController.js
+++ controllers/envsController.js
@@ -1,6 +1,6 @@
 
 const getSimulationUrl = async (req, res) => {
-    res.json({ simulationApiUrl: 'http://' });
+    res.json({ simulationApiUrl: 'https://be35-2800-bf0-284-121-586b-866c-2c36-be84.ngrok-free.app/' });
 }
 
 export {
