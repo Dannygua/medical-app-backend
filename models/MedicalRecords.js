cac97f9596165c1db1c91244253fe82327fab07b
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-18 12:30:53 -0500

    add aptoOperation

--- models/MedicalRecords.js
+++ models/MedicalRecords.js
@@ -26,6 +26,10 @@ const RecordSchema = mongoose.Schema(
       trim: true,
       default: false,
     },
+    esAptoOperation: {
+      type: Boolean,
+      trim: true,
+    },
     recipe: [
       {
         name: {
