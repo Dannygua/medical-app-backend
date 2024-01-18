640d225b97b33d7425cbc78750962bead2b12cda
EzqTrujillo55 <38572422+EzqTrujillo55@users.noreply.github.com>
2024-01-18 09:35:25 -0500

    Update envsController.js

--- controllers/envsController.js
+++ controllers/envsController.js
@@ -1,6 +1,6 @@
 
 const getSimulationUrl = async (req, res) => {
-    res.json({ simulationApiUrl: 'https://44f7-2800-bf0-284-121-cbd-567e-25ef-3272.ngrok-free.app' });   
+    res.json({ simulationApiUrl: 'https://0ec2-2800-bf0-284-121-4484-6509-dc81-fd9.ngrok-free.app' });   
 }
 
 export {
